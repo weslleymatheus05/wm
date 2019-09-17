ac40d4971aadc990dcaf9236ed0eaca8703c1251
Kelion Fernandes Aquino <kelionfa@gmail.com>
2019-09-17 09:54:16 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    //Reconhecer acentuação e caracteres especiais da língua portuguesa
+    //Reconhecer acentuaÃ§Ã£o e caracteres especiais da lÃ­ngua portuguesa
     setlocale(LC_ALL,"Portuguese");
 
     int opcaoDigitada = 0;
@@ -16,18 +16,18 @@ int main()
     do{
 
 
-        printf("\n\t\tSeja Bem-Vindo(a), escolha uma das opções do menu\n"
+        printf("\n\t\tSeja Bem-Vindo(a), escolha uma das opÃ§Ãµes do menu\n"
            "\tMenu\n"
-           "[1] - Imprime na tela se o número digitado é par/impar.\n"
-           "[2] - Potenciação de número de ponto flutuante, resulta em notação científica.\n"
-           "[3] - Radiciação de número de ponto flutante, resultado em notação científica.\n"
-           "[4] - Informa se o ano é bissexto ou não.\n"
-           "[5] - Calcula a média ponderada das notas de 3 provas.\n"
-           "[6] - Calcula a média aritmética das notas de 2 provas.\n"
-           "[10] - Impreme seu nome e sua matrícula, mostra sua matrícula em hexadecimal.\n"
-           "[7] - Lê um número e imprime o fatorial desse número.\n"
-           "[8] - Lê um número e imprime se ele é primo ou não.\n"
-           "[9] - Imprime os n primeiros números da sequência de Fibonacci, onde n é o valor informado por você.\n"
+           "[1] - Imprime na tela se o nÃºmero digitado Ã© par/impar.\n"
+           "[2] - PotenciaÃ§Ã£o de nÃºmero de ponto flutuante, resulta em notaÃ§Ã£o cientÃ­fica.\n"
+           "[3] - RadiciaÃ§Ã£o de nÃºmero de ponto flutante, resultado em notaÃ§Ã£o cientÃ­fica.\n"
+           "[4] - Informa se o ano Ã© bissexto ou nÃ£o.\n"
+           "[5] - Calcula a mÃ©dia ponderada das notas de 3 provas.\n"
+           "[6] - Calcula a mÃ©dia aritmÃ©tica das notas de 2 provas.\n"
+           "[10] - Impreme seu nome e sua matrÃ­cula, mostra sua matrÃ­cula em hexadecimal.\n"
+           "[7] - LÃª um nÃºmero e imprime o fatorial desse nÃºmero.\n"
+           "[8] - LÃª um nÃºmero e imprime se ele Ã© primo ou nÃ£o.\n"
+           "[9] - Imprime os n primeiros nÃºmeros da sequÃªncia de Fibonacci, onde n Ã© o valor informado por vocÃª.\n"
            "[0] - Sair do programa.\n");
 
         //system("cls || clear");
@@ -37,54 +37,54 @@ int main()
 
         switch(opcaoDigitada){
 
-            //Informa se o número é par/impar.
+            //Informa se o nÃºmero Ã© par/impar.
             case 1 :{
 
                 int numDigitado = 0;
 
-                printf("Digite um número inteiro para saber se ele é par/impar: ");
+                printf("Digite um nÃºmero inteiro para saber se ele Ã© par/impar: ");
                 scanf("%d", &numDigitado);
 
                 if(numDigitado%2 == 0){
-                    printf("\n O número %d é par.\n", numDigitado);
+                    printf("\n O nÃºmero %d Ã© par.\n", numDigitado);
                 } else{
-                    printf("\n O número %d é impar.\n", numDigitado);
+                    printf("\n O nÃºmero %d Ã© impar.\n", numDigitado);
                 }
                 break;
             }
-            //Calcula a potenciacão de números com ponto flutuante (vírgula).
+            //Calcula a potenciacÃ£o de nÃºmeros com ponto flutuante (vÃ­rgula).
             case 2 :{
 
                 float baseA = 0;
                 int expoenteB = 0;
                 float resultado = 0;
 
-                printf("Digite um número de ponto flutuante que irá representar a base da potência: ");
+                printf("Digite um nÃºmero de ponto flutuante que irÃ¡ representar a base da potÃªncia: ");
                 scanf("%f", &baseA);
-                printf("\nDigite um número inteiro que irá representar o expoente: ");
+                printf("\nDigite um nÃºmero inteiro que irÃ¡ representar o expoente: ");
                 scanf("%d", &expoenteB);
 
                 resultado = pow(baseA, expoenteB);
 
-                printf("\nResultado da potência em notação científica é: %.3E.", resultado);
+                printf("\nResultado da potÃªncia em notaÃ§Ã£o cientÃ­fica Ã©: %.3E.", resultado);
 
                 break;
             }
-            //Calcula a raiz de números com ponto flutuante em qualquer índice (vírgula).
+            //Calcula a raiz de nÃºmeros com ponto flutuante em qualquer Ã­ndice (vÃ­rgula).
             case 3 :{
 
                 float radicandoA = 0;
                 int indiceB = 0;
                 float resultado = 0;
 
-                printf("Digite o número de ponto flutuante que será o radicando: ");
+                printf("Digite o nÃºmero de ponto flutuante que serÃ¡ o radicando: ");
                 scanf("%f", &radicandoA);
-                printf("Digite o númerio inteiro que será o índice da raiz: ");
+                printf("Digite o nÃºmerio inteiro que serÃ¡ o Ã­ndice da raiz: ");
                 scanf("%d", &indiceB);
 
                 resultado = pow(radicandoA, 1./indiceB);
 
-                printf("\nResultado da raiz em notação científica é: %.3E.", resultado);
+                printf("\nResultado da raiz em notaÃ§Ã£o cientÃ­fica Ã©: %.3E.", resultado);
                 break;
             }
             //Calcula ano bissexto
@@ -98,18 +98,18 @@ int main()
                 if(ano > 0){
                     if(ano%4 == 0){
 
-                        printf("\nO ano de %d é bissexto.", ano);
+                        printf("\nO ano de %d Ã© bissexto.", ano);
                     } else{
 
-                        printf("\nO ano de %d não é bissexto.", ano);
+                        printf("\nO ano de %d nÃ£o Ã© bissexto.", ano);
                     }
                 } else {
 
-                    printf("\nAno inválido.");
+                    printf("\nAno invÃ¡lido.");
                 }
                 break;
             }
-            //Calcula média ponderada de 3 notas.
+            //Calcula mÃ©dia ponderada de 3 notas.
             case 5 :{
 
                 float nota1, nota2, nota3, mediaPonderada;
@@ -125,15 +125,15 @@ int main()
 
                 if(mediaPonderada >= 60){
 
-                    printf("Aluno aprovado com média ponderada de: %.2f.", mediaPonderada);
+                    printf("Aluno aprovado com mÃ©dia ponderada de: %.2f.", mediaPonderada);
 
                 } else{
 
-                    printf("Aluno reprovado com média ponderada de: %.2f.", mediaPonderada);
+                    printf("Aluno reprovado com mÃ©dia ponderada de: %.2f.", mediaPonderada);
                 }
                 break;
             }
-            //Calcula média aritmética de 2 notas.
+            //Calcula mÃ©dia aritmÃ©tica de 2 notas.
             case 6 :{
 
                 float nota1, nota2, mediaAritmetica;
@@ -147,11 +147,11 @@ int main()
 
                     mediaAritmetica = (nota1+nota2)/2;
 
-                    printf("Média das notas informadas é: %.2f", mediaAritmetica);
+                    printf("MÃ©dia das notas informadas Ã©: %.2f", mediaAritmetica);
 
                 } else{
 
-                    printf("Média inválida, não está entre 0.0 e 10.0");
+                    printf("MÃ©dia invÃ¡lida, nÃ£o estÃ¡ entre 0.0 e 10.0");
                 }
                 break;
             }
@@ -162,26 +162,26 @@ int main()
                 int fat = 0;
                 int numFat;
 
-                printf("Digite um número inteiro e maior que zero: ");
+                printf("Digite um nÃºmero inteiro e maior que zero: ");
                 scanf("\n%d", &numFat);
 
-                aux = numFat; //não alterar o valor de numFat
+                aux = numFat; //nÃ£o alterar o valor de numFat
 
                 if(aux > 0){
                     for(fat=1; aux>1; aux--){
                         fat=fat*aux;
                     }
-                    printf("\nO fatorial de %d é: %d", numFat, fat);
+                    printf("\nO fatorial de %d Ã©: %d", numFat, fat);
                 } else{
                     printf("\nErro, valor diferente do esperado.");
                 }
                 break;
             }
-            //Calcula se o número é primo ou não
+            //Calcula se o nÃºmero Ã© primo ou nÃ£o
             case 8 :{
                 int i, numPri, aux = 0;
 
-                printf("Digite o número que você deseja verificar se é primo: ");
+                printf("Digite o nÃºmero que vocÃª deseja verificar se Ã© primo: ");
                 scanf("%d", &numPri);
 
                 for(i = 2; i<numPri; i++){
@@ -194,19 +194,19 @@ int main()
 
                 if(aux == 0){
 
-                    printf("\nO número %d é primo.", numPri);
+                    printf("\nO nÃºmero %d Ã© primo.", numPri);
                 } else{
 
-                    printf("\nO número %d não é primo.", numPri);
+                    printf("\nO nÃºmero %d nÃ£o Ã© primo.", numPri);
                 }
                 break;
             }
-            //Sequência de Fibonacci
+            //SequÃªncia de Fibonacci
             case 9 :{
 
                 int i, indice, ant, prox, soma;
 
-                printf("Digite a quantidade de elementos que você deseja imprimir na sequência de Fibonacci:");
+                printf("Digite a quantidade de elementos que vocÃª deseja imprimir na sequÃªncia de Fibonacci:");
                 scanf("%d", &indice);
 
                 ant = 0;
@@ -214,11 +214,11 @@ int main()
 
                 if(indice >= 0){
 
-                    printf("\nSérie de Fibonacci: %d", prox);
+                    printf("\nSÃ©rie de Fibonacci: %d", prox);
 
                 }else if(indice <= 0){
 
-                    printf("\nSérie de Fibonacci: %d", ant);
+                    printf("\nSÃ©rie de Fibonacci: %d", ant);
                 }
 
                 for(i = 2; i<=indice; i++){
@@ -230,28 +230,30 @@ int main()
                 }
                 break;
             }
-            //Imprime o nome e a matrícula em hexadecimal
+            //Imprime o nome e a matrÃ­cula em hexadecimal
             case 10 :{
 
                 //char nome1 = ;
+                string nome = "Kelion";
                 int matricula = 0;
 
-                printf("Digite sua matrícula: ");
+                printf("Digite sua matrÃ­cula: ");
                 scanf("%d", &matricula);
-
-                printf("\n Sua matrícula em hexadecimal é: %X", matricula);
+                
+                
+                printf("\n Sua matrÃ­cula em hexadecimal Ã©: %X, seu nome Ã© %s", matricula, nome);
                 break;
             }
             //Encerra o programa
             case 0 :{
 
-                printf("\nVocê encerrou o Menu.");
+                printf("\nVocÃª encerrou o Menu.");
                 break;
             }
-            //Caso o usuário digite algo difente de 0-10
+            //Caso o usuÃ¡rio digite algo difente de 0-10
             default :
 
-                printf("\nVocê digitou uma opção inválida, tente novamente.");
+                printf("\nVocÃª digitou uma opÃ§Ã£o invÃ¡lida, tente novamente.");
                 break;
         }
 
@@ -259,7 +261,7 @@ int main()
 
     }while(opcaoDigitada!=0);
 
-    printf("\nGood Bye!\n"); //Aparece quando o usuário apertar a opção 0
+    printf("\nGood Bye!\n"); //Aparece quando o usuÃ¡rio apertar a opÃ§Ã£o 0
 
     return 0;
 }
